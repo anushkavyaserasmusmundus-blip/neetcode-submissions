395bc11225d1e5be85bb0ae7b0a65a0775c21852
Anushka Vyas <anushkavyaserasmusmundus@gmail.com>
2026-09-14 21:19:22 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int trap(int[] height) {
+        int left =0;
+        int right = height.length - 1;
+
+       int leftmax = 0;
+        int rightmax =0;
+        int totalwater = 0;
+
+        while(left<right){
+
+            if(height[left] <= height[right]){
+                //update leftmax
+                if(height[left] >= leftmax){
+                    leftmax = height[left];
+                }
+                else{
+                    totalwater += leftmax - height[left];
+                }
+
+                left++;
+            }
+            else{
+                if(height[right] >= rightmax){
+                    rightmax = height[right];
+                }
+                else{
+                    //water height = smallest bar
+                    totalwater += rightmax - height[right];
+                }
+
+                right--;
+            }
+
+        }
+        return totalwater;
+    }
+}
